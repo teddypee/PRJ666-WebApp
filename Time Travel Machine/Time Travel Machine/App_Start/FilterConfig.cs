8c7b63fb94851b7dbb63d917fbf5ba1915b48b6a
Teddy Prempeh <teddy.prempeh2@gmail.com>
2018-01-30 20:29:03 -0500

    initial

---
+++ Time Travel Machine/Time Travel Machine/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Time_Travel_Machine
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Time Travel Machine/Time Travel Machine/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Time_Travel_Machine.MvcApplication" Language="C#" %>
