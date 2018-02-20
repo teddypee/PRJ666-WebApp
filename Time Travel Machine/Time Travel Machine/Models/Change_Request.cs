023cf651c7aa9473d83b2bc38c6b548ce35f7382
ghkuo <35614591+ghkuo@users.noreply.github.com>
2018-02-20 00:26:34 -0500

    update models manually

--- Time Travel Machine/Time Travel Machine/Models/Change_Request.cs
+++ Time Travel Machine/Time Travel Machine/Models/Change_Request.cs
@@ -9,22 +9,38 @@ namespace Time_Travel_Machine.Models
     {
         public int Change_Request_Id { get; set; }
         public int Region_Id { get; set; }
+        public string regionName { get; set; }
         public int Category_Id { get; set; }
+        public string categoryName { get; set; }
         public int Content_Id { get; set; }
         public string Content_Name { get; set; }
         public int year { get; set; }
         public int userid { get; set; }
+        public string usename { get; set; }
         public DateTime Lastupdatetime { get; set; }
         public int picture_id { get; set; }
-        //public string username { get; set; }
+        public string picname { get; set; }
     }
 
     public class Change_Request : Change_Request_Index
     {
         public string wiki_key { get; set; }
         public string video_key { get; set; }
-
+        public string reason { get; set; }
         public string detail { get; set; }
+        public int request_type { get; set; }
+        public int active { get; set; }
 
+
+    }
+
+    public class CRAddForum
+    {
+        public string Content_Name { get; set; }
+        public int year { get; set; }
+        public string wiki_key { get; set; }
+        public string video_key { get; set; }
+        public string reason { get; set; }
+        public string detail { get; set; }
     }
 }
