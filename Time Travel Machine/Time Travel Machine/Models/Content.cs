ce0c23670bf4e464c77a12b843b77d7b27db03ab
ghkuo <35614591+ghkuo@users.noreply.github.com>
2018-02-27 23:46:05 -0500

    Add files via upload
    update manually w4

---
+++ Time Travel Machine/Time Travel Machine/Models/Content.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Time_Travel_Machine.Models
+{
+    public class ContentIndex
+    {
+        public int Region_Id { get; set; }
+        public string regionName { get; set; }
+        public int Category_Id { get; set; }
+        public string categoryName { get; set; }
+        public int Content_Id { get; set; }
+        public string Content_Name { get; set; }
+        public int year { get; set; }
+        public int userid { get; set; }
+        public string usename { get; set; }
+        public DateTime Lastupdatetime { get; set; }
+        public int picture_id { get; set; }
+        public string picname { get; set; }
+        public string detail { get; set; }
+    }
+
+
+
+    public class Content : ContentIndex
+    {
+        public string wiki { get; set; }
+        public string video { get; set; }
+    }
+}
