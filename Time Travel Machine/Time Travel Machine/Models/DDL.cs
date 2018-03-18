9d7ed6c5249128d223070ba6aff5e67ae28c8232
ghkuo <35614591+ghkuo@users.noreply.github.com>
2018-03-18 03:34:36 -0400

    add manually

---
+++ Time Travel Machine/Time Travel Machine/Models/DDL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Time_Travel_Machine.Models
+{
+    public class YearDDL
+    {
+        public int Year { get; set; }
+        public string description { get; set; }
+        public string YearId { get; set; }
+        public string continentId { get; set; }
+        public string yearddlvalue { get; set; }
+    }
+}
